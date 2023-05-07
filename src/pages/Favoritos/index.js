ecdd0b7d79ddaa4011c964c7941595e6514f8306
aoc <michaelmoranesul@gmail.com>
2023-05-07 16:25:26 -0300

    criando so a estilizacao da rota favoritos

--- src/pages/Favoritos/index.js
+++ src/pages/Favoritos/index.js
@@ -1,10 +1,18 @@
-import styles from "./Favoritos.module.css"
+import Titulo from "components/Titulo";
+import styles from "./Favoritos.module.css";
+import Banner from "components/Banner";
 
 function Favoritos() {
-    return (
-        <>
-        </>
-    )
+  return (
+    <>
+      <Banner imagem="favoritos" />
+      <Titulo>
+         <h1>
+            Meus favoritos
+         </h1>
+      </Titulo>
+    </>
+  );
 }
 
 export default Favoritos;
