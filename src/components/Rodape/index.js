eb727f98063de5c2a6bed637325ea1cebb3edf17
aoc <michaelmoranesul@gmail.com>
2023-05-07 14:16:03 -0300

    component rodape feito por mim como desafio

---
+++ src/components/Rodape/index.js
@@ -0,0 +1,11 @@
+import styles from "./Rodape.module.css"
+
+function Rodape() {
+    return (
+        <p className={styles.paragrafo}>
+            Desenvolvido por Alura & Michael
+        </p>
+    )
+}
+
+export default Rodape
--- src/routes.js
+++ src/routes.js
@@ -1,3 +1,4 @@
+import Rodape from "components/Rodape";
 import Inicio from "pages/Inicio";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
@@ -7,6 +8,7 @@ function AppRoutes() {
             <Routes>
                 <Route path="/" element={<Inicio />}></Route>
             </Routes>
+            <Rodape />
         </BrowserRouter>
     )
 }
