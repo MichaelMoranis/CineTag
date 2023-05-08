f4c8370a4ac775c9742e3e93d6db1d2bb4fc1d48
aoc <michaelmoranesul@gmail.com>
2023-05-07 22:46:23 -0300

    criacao de novos components

--- src/pages/Favoritos/index.js
+++ src/pages/Favoritos/index.js
@@ -1,16 +1,20 @@
 import Titulo from "components/Titulo";
 import styles from "./Favoritos.module.css";
 import Banner from "components/Banner";
+import Card from "components/Card";
 
 function Favoritos() {
   return (
     <>
       <Banner imagem="favoritos" />
       <Titulo>
-         <h1>
+        <h1 className={styles.titulo}>
             Meus favoritos
-         </h1>
+        </h1>
       </Titulo>
+      <section className={styles.container}>
+          <Card id="1" titulo="gato bonifacio" capa="https://github.com/michaelmoranis.png" />
+      </section>
     </>
   );
 }
